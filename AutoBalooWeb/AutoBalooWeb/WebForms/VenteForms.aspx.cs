e3a06e18c81e1bc036b5bc2946b60edc40c68c7d
KevinRatz <kevin.ratz@hotmail.com>
2022-12-19 01:24:50 +0100

    FIN

--- AutoBalooWeb/AutoBalooWeb/WebForms/VenteForms.aspx.cs
+++ AutoBalooWeb/AutoBalooWeb/WebForms/VenteForms.aspx.cs
@@ -25,87 +25,77 @@ namespace AutoBalooWeb.WebForms
             Client cliSess = ((Modele)Session["CoucheModele"]).GetClientVM(Page.User.Identity.Name);
             Vehicule v = ((Modele)Session["CoucheModele"]).GetVehiculeByVM(Session["IdVehicule"].ToString());
             Reservation res = new Reservation(Convert.ToDateTime(txtdtRes.Text), v.IdVoiture, cliSess.Id, 1);
-            
-            //if (((Modele)Session["CoucheModele"]).GetResVM(res))
-            //{
-            //    lit.Text = "Le numéro du chassis de la voiture existe déjà ou des erreurs dans le formulaire";
-            //    ct.Controls.Add(lit);
-            //}
-            //else
-            //// partie paypal
-            //{
-                Session["Reserve"] = res;
-                //tax livraison
-                decimal postageCost = 3.95m;
-                // prix voiture db
-                decimal price = v.Prix;
-                //pour test
-                int qt = 1;
-                decimal subtot = (qt * price);
-                decimal tot = subtot + postageCost;
-                // Authentifier avec paypal
-                var config = new Dictionary<string, string>();
-                config.Add("mode", "sandbox");
-                config.Add("clientId", WebConfigurationManager.AppSettings["clientID"]);
-                config.Add("clientSecret", WebConfigurationManager.AppSettings["clientSecret"]);
-                var accessToken = new OAuthTokenCredential(config).GetAccessToken();
 
-                var apiContext = new APIContext(accessToken);
-                apiContext.Config = config;
-                //info voiture db
-                var carsItem = new Item();
-                carsItem.name = v.Marque.NomMarque+" "+v.Nom;
-                carsItem.currency = "EUR";
-                carsItem.price = price.ToString("0.00", System.Globalization.CultureInfo.InvariantCulture);
+            // partie paypal
+            Session["Reserve"] = res;
+            //tax livraison
+            decimal postageCost = 3.95m;
+            // prix voiture db
+            decimal price = v.Prix;
+            //pour test
+            int qt = 1;
+            decimal subtot = (qt * price);
+            decimal tot = subtot + postageCost;
+            // Authentifier avec paypal
+            var config = new Dictionary<string, string>();
+            config.Add("mode", "sandbox");
+            config.Add("clientId", WebConfigurationManager.AppSettings["clientID"]);
+            config.Add("clientSecret", WebConfigurationManager.AppSettings["clientSecret"]);
+            var accessToken = new OAuthTokenCredential(config).GetAccessToken();
 
-                carsItem.sku = v.ToString().Trim();
-                carsItem.quantity = qt.ToString();
+            var apiContext = new APIContext(accessToken);
+            apiContext.Config = config;
+            //info voiture db
+            var carsItem = new Item();
+            carsItem.name = v.Marque.NomMarque+" "+v.Nom;
+            carsItem.currency = "EUR";
+            carsItem.price = price.ToString("0.00", System.Globalization.CultureInfo.InvariantCulture);
 
-                var transactionDetails = new Details();
-                transactionDetails.tax = "0";
-                transactionDetails.shipping = postageCost.ToString("0.00", System.Globalization.CultureInfo.InvariantCulture);
-                transactionDetails.subtotal = subtot.ToString("0.00", System.Globalization.CultureInfo.InvariantCulture);
+            carsItem.sku = v.ToString().Trim();
+            carsItem.quantity = qt.ToString();
 
-                var transactionAmount = new Amount();
-                transactionAmount.currency = "EUR";
-                transactionAmount.total = tot.ToString("0.00", System.Globalization.CultureInfo.InvariantCulture);
-                transactionAmount.details = transactionDetails;
+            var transactionDetails = new Details();
+            transactionDetails.tax = "0";
+            transactionDetails.shipping = postageCost.ToString("0.00", System.Globalization.CultureInfo.InvariantCulture);
+            transactionDetails.subtotal = subtot.ToString("0.00", System.Globalization.CultureInfo.InvariantCulture);
 
-                var trans = new Transaction();
-                trans.description = "order o cars";
-                //key of res db
-                trans.invoice_number = Guid.NewGuid().ToString();
-                trans.amount = transactionAmount;
-                trans.item_list = new ItemList
-                {
-                    items = new List<Item> { carsItem }
-                };
+            var transactionAmount = new Amount();
+            transactionAmount.currency = "EUR";
+            transactionAmount.total = tot.ToString("0.00", System.Globalization.CultureInfo.InvariantCulture);
+            transactionAmount.details = transactionDetails;
 
-                var payer = new Payer();
-                payer.payment_method = "paypal";
+            var trans = new Transaction();
+            trans.description = "order o cars";
+            //key of res db
+            trans.invoice_number = Guid.NewGuid().ToString();
+            trans.amount = transactionAmount;
+            trans.item_list = new ItemList
+            {
+                items = new List<Item> { carsItem }
+            };
 
-                var redUrls = new RedirectUrls();
-                redUrls.cancel_url = WebConfigurationManager.AppSettings["cancelUrl"];
-                redUrls.return_url = WebConfigurationManager.AppSettings["returnUrl"];
+            var payer = new Payer();
+            payer.payment_method = "paypal";
 
-                var payment = Payment.Create(apiContext, new Payment
-                {
-                    intent = "sale",
-                    payer = payer,
-                    transactions = new List<Transaction> { trans },
-                    redirect_urls = redUrls
-                });
+            var redUrls = new RedirectUrls();
+            redUrls.cancel_url = WebConfigurationManager.AppSettings["cancelUrl"];
+            redUrls.return_url = WebConfigurationManager.AppSettings["returnUrl"];
 
-                Session["paymentId"] = payment.id;
+            var payment = Payment.Create(apiContext, new Payment
+            {
+                intent = "sale",
+                payer = payer,
+                transactions = new List<Transaction> { trans },
+                redirect_urls = redUrls
+            });
 
-                foreach (var link in payment.links)
-                {
-                    if (link.rel.ToLower().Trim().Equals("approval_url"))
-                        Response.Redirect(link.href);
-                }
-            //}
+            Session["paymentId"] = payment.id;
 
-            
+            foreach (var link in payment.links)
+            {
+                if (link.rel.ToLower().Trim().Equals("approval_url"))
+                    Response.Redirect(link.href);
+            }
         }
         protected void BtnCancel_Click(object sender, EventArgs e)
         {
